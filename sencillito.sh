543182c54dbcea7f3dc41a2b082b6448a974e20a
Cristofer Fuentes <contacto@cristoferfb.com>
2020-02-22 01:27:59 -0300

    get debt from file feature ready

--- sencillito.sh
+++ sencillito.sh
@@ -2,19 +2,34 @@
 
 mkdir -p ~/.config/sencillito
 
+# get debt of give commerce code and accountref
+function getDebt () {
+	curl -si https://www.sencillito.com/utilities/$1.json?account_ref=$2 | grep -Eo "\"MONTO\",\"value\"\:\"[0-9]*\"" | grep -o "[0-9]*"
+}
+
+# -s for search code of commerce
 if [ "$1" = "-s" ]; then
 	curl -si https://www.sencillito.com/utilities | grep -Eo "\"code\":[0-9]{4},\"title\":\"[a-zA-Z0-9_]+( [a-zA-Z0-9_\-]+)*\"" | sort -u | grep -i "$2"
 	#TODO fix ticks 
 	exit 0
 fi
 
+# -a for add account to conf file
 if [ "$1" = "-a" ]; then
 	echo "$2 $3" >> ~/.config/sencillito/conf
 	exit 0
 fi
 
-function getDebt () {
-	curl -si https://www.sencillito.com/utilities/$1.json?account_ref=$2 | grep -Eo "\"MONTO\",\"value\"\:\"[0-9]*\"" | grep -o "[0-9]*"
-}
+if [ $# = 0 ]; then
+	if [ -e ~/.config/sencillito/conf ]; then
+		while read line; do
+			getDebt $line
+		done < ~/.config/sencillito/conf
+
+		exit 0
+	fi
+	exit 0
+fi
+
 
 getDebt $1 $2
