14b6054c1330b52f856fb0a561de9b642184998d
Cristofer Fuentes <contacto@cristoferfb.com>
2020-02-22 01:40:15 -0300

    intall makefile

---
+++ makefile
@@ -0,0 +1,5 @@
+install: sencillito.sh
+	cp sencillito.sh /usr/bin/sencillito
+
+uninstall: /usr/bin/sencillito
+	rm /usr/bin/sencillito
--- sencillito.sh
+++ sencillito.sh
@@ -7,19 +7,27 @@ function getDebt () {
 	curl -si https://www.sencillito.com/utilities/$1.json?account_ref=$2 | grep -Eo "\"MONTO\",\"value\"\:\"[0-9]*\"" | grep -o "[0-9]*"
 }
 
-# -s for search code of commerce
+# -s search code of commerce
 if [ "$1" = "-s" ]; then
 	curl -si https://www.sencillito.com/utilities | grep -Eo "\"code\":[0-9]{4},\"title\":\"[a-zA-Z0-9_]+( [a-zA-Z0-9_\-]+)*\"" | sort -u | grep -i "$2"
 	#TODO fix ticks 
 	exit 0
 fi
 
-# -a for add account to conf file
+# -a add account to conf file
 if [ "$1" = "-a" ]; then
 	echo "$2 $3" >> ~/.config/sencillito/conf
 	exit 0
 fi
 
+# -h show help info
+if [ "$1" = "-h" ]; then
+	echo help
+	#TODO help
+	exit 0
+fi
+
+# no argument read accounts from conf file if exist
 if [ $# = 0 ]; then
 	if [ -e ~/.config/sencillito/conf ]; then
 		while read line; do
@@ -28,8 +36,8 @@ if [ $# = 0 ]; then
 
 		exit 0
 	fi
+	#TODO show help
 	exit 0
 fi
-
 
 getDebt $1 $2
