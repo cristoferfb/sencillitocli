f67ff30c8a75e29ded85e198d0a951e98a42b4bf
Cristofer Fuentes <contacto@cristoferfb.com>
2020-02-22 01:11:45 -0300

    get debt feature ready, write account to file feature ready

--- sencillito.sh
+++ sencillito.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+mkdir -p ~/.config/sencillito
 
 if [ "$1" = "-s" ]; then
 	curl -si https://www.sencillito.com/utilities | grep -Eo "\"code\":[0-9]{4},\"title\":\"[a-zA-Z0-9_]+( [a-zA-Z0-9_\-]+)*\"" | sort -u | grep -i "$2"
 	#TODO fix ticks 
+	exit 0
 fi
+
+if [ "$1" = "-a" ]; then
+	echo "$2 $3" >> ~/.config/sencillito/conf
+	exit 0
+fi
+
+function getDebt () {
+	curl -si https://www.sencillito.com/utilities/$1.json?account_ref=$2 | grep -Eo "\"MONTO\",\"value\"\:\"[0-9]*\"" | grep -o "[0-9]*"
+}
+
+getDebt $1 $2
